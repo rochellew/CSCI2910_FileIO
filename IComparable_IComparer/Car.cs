3514d47feccb2191a1360afb62a84b466e0d3de3
rochellew <32746298+rochellew@users.noreply.github.com>
2022-06-02 00:59:30 -0400

    Add files via upload

---
+++ IComparable_IComparer/Car.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+
+namespace IComparable_IComparer
+{
+    public class Car : IComparable
+    {
+        // nested classes
+
+        #region NestedClasses
+        // sort ascending by year (on Year property)
+        private class SortYearAscendingHelper : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                Car c1 = (Car)a;
+                Car c2 = (Car)b;
+
+                if (c1.Year > c2.Year)
+                    return 1;
+                if (c1.Year < c2.Year)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+
+        // sort descending on Year property
+        private class SortYearDescendingHelper : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                Car c1 = (Car)a;
+                Car c2 = (Car)b;
+
+                if (c1.Year < c2.Year)
+                    return 1;
+                if (c1.Year > c2.Year)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+
+        // descending sort on Make property
+        private class SortMakeDescendingHelper : IComparer
+        {
+            int IComparer.Compare(object? a, object? b)
+            {
+                Car? car1 = a as Car;
+                Car? car2 = b as Car;
+                return String.Compare(car2.Make, car1.Make);
+            }
+        }
+        #endregion
+
+        public string Make { get; set; }
+        public int Year { get; set; }
+
+        public Car(string make, int year)
+        {
+            Make = make;
+            Year = year;
+        }   
+
+        // Implements CompareTo method of IComparable interface
+        int IComparable.CompareTo(object? obj)
+        {
+            Car c = (Car)obj;
+            return String.Compare(this.Make, c.Make);
+
+        }
+
+        // Methods that will return IComparer objects for sort helpers
+        public static IComparer SortYearAcending()
+        {
+            return (IComparer) new SortYearAscendingHelper();
+        }
+
+        public static IComparer SortYearDescending()
+        {
+            return new SortYearDescendingHelper() as IComparer;
+        }
+
+        public static IComparer SortMakeDescending()
+        {
+            return new SortMakeDescendingHelper();
+        }
+    }
+
+
+}
---
+++ IComparable_IComparer/Program.cs
@@ -0,0 +1,45 @@
+﻿// IComparable & IComparer
+using IComparable_IComparer;
+
+
+// create an array of Car objects
+Car[] arrayOfCars = new Car[6]
+{
+    new Car("Ford", 1992),
+    new Car("Fiat", 1988),
+    new Car("Honda", 1932),
+    new Car("Chevy", 1999),
+    new Car("Mazda", 1967),
+    new Car("Dodge", 1981),
+};
+
+Console.WriteLine("Array - Unsorted\n");
+foreach(Car car in arrayOfCars)
+    Console.WriteLine(car.Make + "\t\t" + car.Year);
+
+// Demo IComparable by sorting array with "default" sort order
+Console.WriteLine("\nArray - Sorted by Make (Ascending - IComparable)\n");
+Array.Sort(arrayOfCars);
+foreach (Car car in arrayOfCars)
+    Console.WriteLine(car.Make + "\t\t" + car.Year);
+
+// Demo ascending sort of numeric values with IComparer
+Console.WriteLine("\nArray - Sorted by Year (Ascending - IComparer)\n");
+Array.Sort(arrayOfCars, Car.SortYearAcending());
+foreach (Car car in arrayOfCars)
+    Console.WriteLine(car.Make + "\t\t" + car.Year);
+
+// Demo descending sort of numeric values with IComparer
+Console.WriteLine("\nArray - Sorted by Year (Descending - IComparer)\n");
+Array.Sort(arrayOfCars, Car.SortYearDescending());
+foreach (Car car in arrayOfCars)
+    Console.WriteLine(car.Make + "\t\t" + car.Year);
+
+// Demo descending sort of string values with IComparer
+Console.WriteLine("\nArray - Sorted by Make (Descending - IComparer)\n");
+Array.Sort(arrayOfCars, Car.SortMakeDescending());
+foreach (Car car in arrayOfCars)
+    Console.WriteLine(car.Make + "\t\t" + car.Year);
+
+
+
